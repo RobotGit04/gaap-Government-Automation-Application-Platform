84ca96cb27151cf5e6fda98ff796e10b8f2b413e
nikhilesh <nikhilrobot04@gmail.com>
2026-03-13 05:07:53 +0530

    fix: move types to dependencies, fix AuthRequest

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,6 +1,8 @@
 import { Request } from 'express';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { ParsedQs } from 'qs';
 
-export interface AuthRequest extends Request {
+export interface AuthRequest extends Request<ParamsDictionary, any, any, ParsedQs> {
   user?: {
     id: string;
     email: string;
